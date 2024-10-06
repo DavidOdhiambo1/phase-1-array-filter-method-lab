ae47f494108553ced2ec89934af3c2dde89d32ff
DavidOdhiambo1 <davidworkmail2021@gmail.com>
2024-10-06 13:20:13 +0300

    .filter() test

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, name){
+    return array.filter(element => element.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(array, name){
+    return array.filter(element => element.slice(0,2)===name.slice(0,2) )
+}
+
+function matchName(array, reqName){
+    return array.filter(property => property.name === reqName)
+}
